3e5f2d76a865c48856f1a516aa7939438f6dea3f
RichardHightower <richard.m.hightower@gmail.com>
2017-08-20 14:27:56 -0700

    Adding JSON spray support

--- userService/src/main/scala/com/rick/user/Model.scala
+++ userService/src/main/scala/com/rick/user/Model.scala
@@ -8,3 +8,13 @@ case class Tags(tags: Seq[String])
 
 case class UserResponse(userAlias: String, tags: Seq[String])
 
+
+import spray.json._
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val userIdFormat = jsonFormat1(UserId)
+  implicit val userAliasFormat = jsonFormat1(UserAlias)
+}
+
